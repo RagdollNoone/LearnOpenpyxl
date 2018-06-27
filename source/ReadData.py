2be3c8e5e6fd9c4f9d6f0fc5b660009b02eba9f1
RagdollNoone <RagdollNoone@163.com>
2018-06-27 15:06:46 +0800

    stage commit

--- source/ReadData.py
+++ source/ReadData.py
@@ -7,13 +7,15 @@ import shutil
 
 class ReadData():
     rootDir = '../Resource/input'
+
     targetName = 'target.xlsx'
-    targetStringArr = ['Strongly Disagree',
-                       'Disagree',
-                       'Slightly Disagree',
-                       'Slightly Agree',
-                       'Agree',
-                       'Strongly Agree']
+    targetStringArr = {'Strongly Disagree': 0,
+                       'Disagree': 20,
+                       'Slightly Disagree': 40,
+                       'Slightly Agree': 60,
+                       'Agree': 80,
+                       'Strongly Agree': 100}
+
     source_col = 'C'
     source_begin_row = 16
     source_end_row = 30
@@ -33,7 +35,6 @@ class ReadData():
 
         for i in range(0, len(file_list)):
             path = os.path.join(file_dir, file_list[i])
-            base_name = os.path.basename(path)
 
             if os.path.basename(path) == self.targetName:
                 continue
@@ -48,7 +49,7 @@ class ReadData():
                 wb = load_workbook(path)
                 ws = wb.active
 
-                for j in range(0, self.source_end_row - self.source_begin_row):
+                for j in range(0, self.source_end_row - self.source_begin_row + 1):
                     read_unit = self.source_col + str(j + self.source_begin_row)
 
                     if self.check_evaluate_string_legal(ws[read_unit].value):
@@ -69,8 +70,9 @@ class ReadData():
                         ws_write = wb_write.active
 
                         write_unit = self.target_col + str(j + self.target_begin_row)
-                        # ws_write[write_unit] = ws[read_unit].value
-                        ws_write.cell(write_unit).value = ws[read_unit].value
+
+                        ws_write[write_unit] = self.get_evaluate_value(ws[read_unit].value)
+                        wb_write.template = False
                         wb_write.save(target_file_path)
 
                     else:
@@ -82,11 +84,18 @@ class ReadData():
     def check_evaluate_string_legal(self, evaluate):
         result = False
 
-        for value in self.targetStringArr:
-            if value == evaluate:
+        for key in self.targetStringArr:
+            if key == evaluate:
                 return True
 
         return result
+
+    def get_evaluate_value(self, evaluate):
+        for key in self.targetStringArr:
+            if key == evaluate:
+                return self.targetStringArr[key]
+
+        return -1
 
 
 read_data = ReadData()
